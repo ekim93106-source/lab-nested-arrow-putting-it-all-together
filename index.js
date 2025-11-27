e8d3379d81108953100cdbc655603f33cd4f9eef
kimani <ekim93106@gmail.com>
2025-11-27 15:18:51 +0300

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
+function createLoginTracker(userInfo) {
+    let attemptCount = 0;
 
+    const loginAttempt = (passwordAttempt) => {
+        attemptCount++;
+
+        if (attemptCount > 3) {
+            return "Account locked due to too many failed login attempts";
+        }
+
+        if (passwordAttempt === userInfo.password) {
+            return "Login successful";
+        } else {
+            return `Attempt ${attemptCount}: Login failed`;
+        }
+    };
+
+    return loginAttempt;
+}
 
 
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+    ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
+
+
+
+
